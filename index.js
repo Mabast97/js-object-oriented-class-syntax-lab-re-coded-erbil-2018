4136880253c5bb3cca5ed33923ed694ec5305741
Mabast Hashm Jamel <mabasthashm1@gmail.com>
2018-07-31 21:07:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,13 +17,6 @@ class BoardMember
 
 class Ceo extends BoardMember
 {
-  constructor(name, homeState, training)
-  {
-    this.name = name;
-    this.homeState = homeState;
-    this.training = training;
-    
-  }
   hireEmployee(){ return "Welcome aboard!"; }
 }
 
