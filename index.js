05f0c060603e36e6c97d22bce2c1c495bab3645a
Mabast Hashm Jamel <mabasthashm1@gmail.com>
2018-07-31 20:59:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ class BoardMember
   approve() { return "You can do that!"; }
   doCharity() { return "I like to help people."; }
   releasePressStatement() { return "You will see great things from Scuber."; }
-  sayHi() { ; }
+  sayHi() { return "Hi, my name is "+this.name+". I am from "this.homestate>, and I was trained in <training>.; }
   
 }
 
